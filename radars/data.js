3cb28951d014bac2ff5d705f75ff59f4b1a24dcf
filipesperandio <filipesperandio>
2011-05-25 18:47:01 -0300

    adding radar to the site

---
+++ radars/data.js
@@ -0,0 +1,46 @@
+title = 'Rafael Ferreira, May 2011';
+
+data = {
+    'Languages': {
+        'Java': 3,
+        'C#': 4,
+        'Objective-C': 1,
+        'C': 1,
+        'Ruby': 2,
+        'Javascript': 3,
+    },
+
+    'Tools': {
+        'EC2': 2,
+        'SDB': 2,
+        'S3': 2,
+        'Ivy': 2,
+        'Ant': 3,
+        'Visual Studio': 4,
+        'IntelliJ': 2,
+        'Eclipse': 3,
+        'Git': 2,
+        'Vim': 2,
+        'Svn': 2,
+    },
+
+    'Techniques': {
+        'TDD': 3,
+        'DDD': 2,
+        'BDD': 2,
+        'REST':1,
+        'OOD': 4,
+        'DevOps': 1,
+        'Refactoring': 3,
+    },
+
+    'Platforms': {
+        'Linux': 3,
+        'MacOS X': 2,
+        'Windows': 4,
+        'iOS': 1,
+        'Android': 1,
+        '.Net': 3,
+    }
+};
+
