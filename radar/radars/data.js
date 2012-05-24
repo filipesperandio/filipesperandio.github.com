4eef057b92fefa81f88c53cfcf11d306e715f2d5
Filipe Gomes Esperandio <filipesperandio@gmail.com>
2012-05-24 15:09:38 -0300

    updating interests and knowledge

--- radar/radars/data.js
+++ radar/radars/data.js
@@ -1,67 +1,72 @@
 title = 'Filipe Esperandio, May 2011';
 
 data = {
-    'Languages': {
-        'Java': 4,
-	'JavaScript':1,
-	'ShellScript': 2,
-        'Ruby': 1,
-        'Python': 1,
-        'C': 1,
-    },
+  'Languages': {
+    'Java': 4,
+    'JavaScript': 3,
+    'ShellScript': 2,
+    'Ruby': 2,
+    'Python': 1,
+  },
 
-    'Tools': {
-        'Ant':3,
-        'Eclipse':4,
-        'Git':3,
-	'Oracle':2,
-        'Svn':3,
-	'Spring':2,
-        'EC2':  1,
-	'Axis2':2,
-	'Hibernate':2,
-	'Mokito':3,
-	'Jmock':3,
-        'Maven':1,
-	'iBatis':2,
-        'Ivy':2,
-        'Vim':2,
-	'SoapUI':2,
-	'GAE':2,
-    },
+  'Tools': {
+    'Eclipse':4,
+    'Git':4,
+    'Vim':3,
+    'Oracle':2,
+    'Svn':3,
+    'AWS':  2,
+    'S3':  2,
+    'Ant':3,
+    'VRaptor':4,
+    'Restfulie':3,
+    'Axis2':2,
+    'Spring':2,
+    'Hibernate':2,
+    'Mokito':3,
+    'Jmock':3,
+    'Maven':3,
+    'iBatis':2,
+    'Jasmine':2,
+    'jQuery':2,
+    'AngularJS':3,
+    'Bootstrap':3,
+    'MongoDB':2,
+    'GAE':1,
+    'Ivy':2,
+    'SoapUI':2,
+  },
 
-    'Techniques': {
-        'DDD': 1,
-        'TDD': 3,
-        'BDD': 1,
-        'OOD': 4,
-        'Refactoring':2,
-        'DevOps':3,
-    },
+  'Techniques': {
+    'DDD': 3,
+    'TDD': 3,
+    'BDD': 1,
+    'OOD': 4,
+    'Refactoring':3,
+    'DevOps':3,
+  },
 
-    'Platforms': {
-        'Linux':4,
-        'Windows':4,
-	'ESX/vSphere':2,
-	'Android':1,
-    }
+  'Platforms': {
+    'MacOS':3,
+    'Linux':4,
+    'Windows':4,
+    'ESX/vSphere':2,
+    'Heroku':3,
+    'Android':1,
+  }
 };
 
 interest = {
-    'Languages': {
-        'C#': 1,
-    },
+  'Languages': {
+  },
 
-    'Tools': {
-        'IntelliJ':2,
-    },
+  'Tools': {
+  },
 
-    'Techniques': {
-    },
+  'Techniques': {
+  },
 
-    'Platforms': {
-        'MacOS X':1,
-	
-    }
+  'Platforms': {
+  }
 };
 
